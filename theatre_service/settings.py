b02ab3d665f9fbb78a98c5a638e825c840e41abf
asdadaversa <peikqudakeus@gmail.com>
2023-12-29 16:05:07 +0200

    implement JWT Authentication

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -138,3 +138,9 @@ AUTH_USER_MODEL = "user.User"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
